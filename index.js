470be25a914203a9c94db552775554df938a7ecf
Tonye Jack <tonye@coherentpath.com>
2019-08-29 19:21:07 -0400

    Updated the install scripts.

--- index.js
+++ index.js
@@ -1,24 +1,17 @@
 'use strict';
 
+const npmRun = require('npm-run');
 const argv = require('yargs')
   .option('config', { alias: 'c', default: 'codegen.yml' })
   .option('verbose', { alias: 'v', default: false }).argv;
 
-const { spawn } = require('child_process');
-const codeGen = spawn('graphql-codegen', ['--config', argv.config]);
-
-codeGen.stdout.on('data', (data) => {
-  console.log(`stdout: ${data}`);
-});
-
-codeGen.on('close', (code) => {
+npmRun.exec('graphql-codegen', {cwd: __dirname}, function (err, stdout, stderr) {
+  console.log(`stdout: ${stdout}`);
   if (argv.verbose) {
-    console.log(`child process close all stdio with code ${code}`);
-  }
-});
-
-codeGen.on('exit', (code) => {
-  if (argv.verbose) {
-    console.log(`child process exited with code ${code}`);
+    if (err) {
+      console.log(`Failed to start codeGen. ${err.message}`);
+      console.error(err);
+    }
+    console.log(stderr);
   }
 });
