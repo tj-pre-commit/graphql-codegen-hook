a525eb97b72481b6c1658dcf6eec96d288767813
Tonye Jack <tonye@coherentpath.com>
2019-08-29 21:35:10 -0400

    Added exception handling.

--- bin/graphql-codegen-hook.js
+++ bin/graphql-codegen-hook.js
@@ -3,16 +3,27 @@
 'use strict';
 
 const path = require('path');
+const { createConfig } = require('@graphql-codegen/cli/dist/commonjs/config');
 const process = require('process');
 const argv = require('yargs')
   .option('config', { alias: 'c', required: true })
   .option('verbose', { alias: 'v', default: false }).argv;
 
 
-const { executeCodegen, generate } = require('@graphql-codegen/cli');
+const { generate } = require('@graphql-codegen/cli');
 
 const configPath = path.resolve(process.cwd(), argv.config);
 
-generate({
-  configFilePath: configPath,
+const config = createConfig({ config: configPath });
+
+config.then(config => {
+  return generate(config)
+    .then(() => {
+      process.exit(0);
+    })
+    .catch(error => {
+      process.exit(1);
+    });
+}).catch(error => {
+  process.exit(1);
 });
