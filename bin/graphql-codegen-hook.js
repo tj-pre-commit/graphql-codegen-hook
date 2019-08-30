9876394671aec024851e126424ff01e9e00d0269
Tonye Jack <tonye@coherentpath.com>
2019-08-29 21:11:35 -0400

    Fixed execution.

--- bin/graphql-codegen-hook.js
+++ bin/graphql-codegen-hook.js
@@ -3,26 +3,16 @@
 'use strict';
 
 const npmRun = require('npm-run');
+const path = require('path');
 const argv = require('yargs')
   .option('config', { alias: 'c', required: true })
   .option('verbose', { alias: 'v', default: false }).argv;
 
-var child = npmRun.spawn('graphql-codegen', [`${argv.config}`], {cwd: __dirname});
 
+const { executeCodegen, generate } = require('@graphql-codegen/cli');
 
-child.stdout.on('data', (data) => {
-  console.log(`stdout: ${data}`);
-});
+const configPath = path.resolve(process.cwd(), argv.config);
 
-
-child.stderr.on('data', (data) => {
-  console.error(`stderr: ${data}`);
-  process.exit(1);
-});
-
-
-child.on('close', (code) => {
-  if (code !== 0) {
-    console.log(`graphql-codegen-hook process exited with code ${code}`);
-  }
+executeCodegen({
+  configFilePath: configPath,
 });
