29c57503415e07e358c5163c6f6d30431a6c5d7d
Tonye Jack <tonye@coherentpath.com>
2019-08-29 20:58:13 -0400

    updated the script.

--- bin/graphql-codegen-hook.js
+++ bin/graphql-codegen-hook.js
@@ -4,16 +4,25 @@
 
 const npmRun = require('npm-run');
 const argv = require('yargs')
-  .option('config', { alias: 'c', default: 'codegen.yml' })
+  .option('config', { alias: 'c', required: true })
   .option('verbose', { alias: 'v', default: false }).argv;
 
-npmRun.exec('graphql-codegen', {cwd: __dirname}, function (err, stdout, stderr) {
-  console.log(`stdout: ${stdout}`);
-  if (err) {
-    console.log(`Failed to start codeGen. ${err.message}`);
-    if (argv.verbose) {
-      console.error(err);
-      console.log(stderr);
-    }
+var child = npmRun.spawn('graphql-codegen', [`${argv.config}`], {cwd: __dirname});
+
+
+child.stdout.on('data', (data) => {
+  console.log(`stdout: ${data}`);
+});
+
+
+child.stderr.on('data', (data) => {
+  console.error(`stderr: ${data}`);
+  process.exit(1);
+});
+
+
+child.on('close', (code) => {
+  if (code !== 0) {
+    console.log(`graphql-codegen-hook process exited with code ${code}`);
   }
 });
