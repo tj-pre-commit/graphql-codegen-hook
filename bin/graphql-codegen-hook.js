12cebe254642082722638bbeb71664f535926b62
Tonye Jack <tonye@coherentpath.com>
2019-12-28 07:06:13 -0500

    Added support for parsing the filenames.

--- bin/graphql-codegen-hook.js
+++ bin/graphql-codegen-hook.js
@@ -3,18 +3,30 @@
 'use strict';
 
 const argv = require('yargs')
-  .option('config', { alias: 'c', required: true })
-  .option('verbose', { alias: 'v', default: false }).argv;
+  .option('config', { alias: 'c', required: true, type: 'string' })
+  .option('verbose', { alias: 'v', default: false, type: 'boolean' })
+  .command('$0 [filenames..]', 'Changed filenames', {})
+  .help()
+  .argv;
 
+const { filenames = [] } = argv;
+const shouldExecute = filenames.filter(minimatch.filter("*.+(tsx?|jsx?)", { matchBase: true })).length > 0;
 
 const shell = require('shelljs');
 const basePath = shell.pwd().toString();
 
 shell.cd(basePath);
 
-var child = shell.exec(`npx graphql-codegen --config ${argv.config}`, { async: true, verbose: argv.verbose });
-
-child.stderr.on('data', function (data) {
-  console.error(data);
-  shell.exit(1);
-});
+if (!shouldExecute) {
+  shell.exit(0);
+} else {
+  const child = shell.exec(`npx graphql-codegen --config ${argv.config}`, {
+    async: true,
+    verbose: argv.verbose
+  });
+  
+  child.stderr.on('data', function (data) {
+    console.error(data);
+    shell.exit(1);
+  });
+}
