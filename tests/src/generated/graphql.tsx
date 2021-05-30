7d748d69de992b2b04404430bca1013e15320acd
Tonye Jack <jtonye@ymail.com>
2021-05-30 12:44:30 +0000

    Fixed security vulnerabilities.

--- tests/src/generated/graphql.tsx
+++ tests/src/generated/graphql.tsx
@@ -1,15 +1,12 @@
 /* eslint-disable */
 /* tslint:disable */
-import gql from 'graphql-tag';
-import * as React from 'react';
-import * as ApolloReactCommon from '@apollo/react-common';
-import * as ApolloReactComponents from '@apollo/react-components';
-import * as ApolloReactHoc from '@apollo/react-hoc';
-import * as ApolloReactHooks from '@apollo/react-hooks';
+import { gql } from '@apollo/client';
+import * as Apollo from '@apollo/client';
 export type Maybe<T> = T | null;
-export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
-
-
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+const defaultOptions =  {}
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
   ID: string;
@@ -20,16 +17,16 @@ export type Scalars = {
 };
 
 export type IsUpdatingType = {
-   __typename?: 'IsUpdatingType';
+  __typename?: 'IsUpdatingType';
   isUpdating: Scalars['Boolean'];
 };
 
 export type Query = {
-   __typename?: 'Query';
+  __typename?: 'Query';
   isUpdating: Scalars['Boolean'];
 };
 
-export type IsUpdatingQueryVariables = {};
+export type IsUpdatingQueryVariables = Exact<{ [key: string]: never; }>;
 
 
 export type IsUpdatingQuery = (
@@ -43,29 +40,12 @@ export const IsUpdatingDocument = gql`
   isUpdating @client
 }
     `;
-export type IsUpdatingComponentProps = Omit<ApolloReactComponents.QueryComponentOptions<IsUpdatingQuery, IsUpdatingQueryVariables>, 'query'>;
-
-    export const IsUpdatingComponent = (props: IsUpdatingComponentProps) => (
-      <ApolloReactComponents.Query<IsUpdatingQuery, IsUpdatingQueryVariables> query={IsUpdatingDocument} {...props} />
-    );
-    
-export type IsUpdatingProps<TChildProps = {}> = ApolloReactHoc.DataProps<IsUpdatingQuery, IsUpdatingQueryVariables> & TChildProps;
-export function withIsUpdating<TProps, TChildProps = {}>(operationOptions?: ApolloReactHoc.OperationOption<
-  TProps,
-  IsUpdatingQuery,
-  IsUpdatingQueryVariables,
-  IsUpdatingProps<TChildProps>>) {
-    return ApolloReactHoc.withQuery<TProps, IsUpdatingQuery, IsUpdatingQueryVariables, IsUpdatingProps<TChildProps>>(IsUpdatingDocument, {
-      alias: 'isUpdating',
-      ...operationOptions
-    });
-};
 
 /**
  * __useIsUpdatingQuery__
  *
  * To run a query within a React component, call `useIsUpdatingQuery` and pass it any options that fit your needs.
- * When your component renders, `useIsUpdatingQuery` returns an object from Apollo Client that contains loading, error, and data properties 
+ * When your component renders, `useIsUpdatingQuery` returns an object from Apollo Client that contains loading, error, and data properties
  * you can use to render your UI.
  *
  * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
@@ -76,12 +56,14 @@ export function withIsUpdating<TProps, TChildProps = {}>(operationOptions?: Apol
  *   },
  * });
  */
-export function useIsUpdatingQuery(baseOptions?: ApolloReactHooks.QueryHookOptions<IsUpdatingQuery, IsUpdatingQueryVariables>) {
-        return ApolloReactHooks.useQuery<IsUpdatingQuery, IsUpdatingQueryVariables>(IsUpdatingDocument, baseOptions);
+export function useIsUpdatingQuery(baseOptions?: Apollo.QueryHookOptions<IsUpdatingQuery, IsUpdatingQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<IsUpdatingQuery, IsUpdatingQueryVariables>(IsUpdatingDocument, options);
       }
-export function useIsUpdatingLazyQuery(baseOptions?: ApolloReactHooks.LazyQueryHookOptions<IsUpdatingQuery, IsUpdatingQueryVariables>) {
-          return ApolloReactHooks.useLazyQuery<IsUpdatingQuery, IsUpdatingQueryVariables>(IsUpdatingDocument, baseOptions);
+export function useIsUpdatingLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<IsUpdatingQuery, IsUpdatingQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<IsUpdatingQuery, IsUpdatingQueryVariables>(IsUpdatingDocument, options);
         }
 export type IsUpdatingQueryHookResult = ReturnType<typeof useIsUpdatingQuery>;
 export type IsUpdatingLazyQueryHookResult = ReturnType<typeof useIsUpdatingLazyQuery>;
-export type IsUpdatingQueryResult = ApolloReactCommon.QueryResult<IsUpdatingQuery, IsUpdatingQueryVariables>;
+export type IsUpdatingQueryResult = Apollo.QueryResult<IsUpdatingQuery, IsUpdatingQueryVariables>;
